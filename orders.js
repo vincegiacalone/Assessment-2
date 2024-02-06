a4518f81292feb32498e8db4d6ebadda6d3f4890
Vincent Giacalone <vgiacalone89@gmail.com>
2024-02-06 14:39:33 -0500

    finished orders

--- orders.js
+++ orders.js
@@ -31,6 +31,19 @@
 
 //CODE HERE
 
+class Ticket {
+    constructor(items, orderTime, customerId) {
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
+
+    updateStatus(newStatus) {
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is now ${this.status}.`);
+    }
+}
 
 
 /*
@@ -46,6 +59,7 @@
 
 //CODE HERE
 
+const firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM', '575');
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +68,5 @@
 */
 
 //CODE HERE
+
+firstTicket.updateStatus('cooking');
